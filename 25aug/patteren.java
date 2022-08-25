abf89e2bab9968c24e5f51ec3f1f71c19ed5208b
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-08-25 18:07:36 +0530

    Add files via upload

---
+++ 25aug/patteren.java
@@ -0,0 +1,25 @@
+/*				*
+               ***
+              *****
+             *******
+            *********
+           ***********
+*/
+package Java;
+import java.util.Scanner;
+public class patteren {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.print("Enter the number of rows you want to print   ");
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=n;j>i;j--) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=(2*i)-1;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
