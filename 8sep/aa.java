31f3a9a50cb43b007fa98ce63eed22a8792b4964
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-09-09 17:01:13 +0530

    Add files via upload

---
+++ 8sep/aa.java
@@ -0,0 +1,24 @@
+/*Q.2... Write a java class to develop an employee class object using 
+constructor.
+The constructor hold initialized the emp name & emp Id for 5 Emp
+now write another class having name and from this class you have to 
+create 5 employee objects but 
+you are not allowed to use new keyword.*/
+
+package com.innovation.shapes;
+ abstract class A{
+	
+	 static void sete(int id, String name) {		
+		 System.out.println("The id of the emp is:  " +id+  "   and name is  " +name);
+		 
+}
+}
+public class aa {
+	public static void main(String[] args) {
+		A.sete(23, "Sarbojit");		//calling without creating new keyword
+		A.sete(24, "Rohan");
+		A.sete(21, "ABC");
+		A.sete(22, "CDF");
+		A.sete(25, "XYZ");
+	}
+}
