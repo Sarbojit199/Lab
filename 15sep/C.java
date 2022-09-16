7b4caf2a1d95ce64c140c69035e0d70323935b58
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-09-16 14:11:33 +0530

    Add files via upload

---
+++ 15sep/C.java
@@ -0,0 +1,13 @@
+/* Class A serves as a base class for the derived class B, which in turn 
+serves as a base class for the derived class C. (Which type Of Inheritance 
+explain with an example)*/
+class r{
+}
+class B extends r{
+		
+	}
+
+public class C extends B{
+
+}
+/*Multilevel inheritance*/
