44964f7dc3bfeeb9faaa67d4435bfafdb99fb92f
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-11-17 18:27:03 +0530

    Add files via upload

---
+++ 17Nov/ReplaceArray.java
@@ -0,0 +1,25 @@
+/* Write a Java program to replace an element in a linked list */
+
+package Collections;
+
+import java.util.LinkedList;
+
+public class ReplaceArray {
+	
+	public static void main(String[] args) {
+		LinkedList<Integer> l1 = new LinkedList<Integer>();		//creating new arraylist object
+		
+
+		
+		l1.add(5);				//adding the element
+		l1.add(10);
+		l1.add(6);
+		System.out.println("before insert the list is " +l1);
+		l1.set(1, 9);   //replacing the element
+		System.out.println("after inserting the list is" +l1);
+		
+		
+		
+	}
+
+}
