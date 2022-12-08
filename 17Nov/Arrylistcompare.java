e7ed9e5833866934a8b458cb9de887aaf7d6339f
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-12-08 15:24:53 +0530

    Update Arrylistcompare.java

--- 17Nov/Arrylistcompare.java
+++ 17Nov/Arrylistcompare.java
@@ -7,19 +7,21 @@ import java.util.List;
 
 public class Arrylistcompare {
 public static void main(String[] args) {
+	
+	//make arraylist  and adding the string
 		List<String> all = new ArrayList<String>();				//aarraylist
 		all.add("you");											//adding in list
 		all.add("me");
 		all.add("they");
 		all.add("them");
 		
-		
+		//adding values in second arraylist
 		List<String> al2 = new ArrayList<String>();					//arraylist
 		al2.add("story");					
 		al2.add("of");
 		al2.add("my");
 		al2.add("journey");
-		
+		//compare if both content equals or not
 		System.out.println("Yes content are equal  " +(all.toString().contentEquals(al2.toString())));			//compare
 
 		
