91df25b73824be6c99d9cd33dc9a5cc26bc1bb64
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-12-08 15:28:10 +0530

    Update ReplaceArray.java

--- 17Nov/ReplaceArray.java
+++ 17Nov/ReplaceArray.java
@@ -10,11 +10,12 @@ public class ReplaceArray {
 		LinkedList<Integer> l1 = new LinkedList<Integer>();		//creating new arraylist object
 		
 
-		
+		//adding into the linked list
 		l1.add(5);				//adding the element
 		l1.add(10);
 		l1.add(6);
 		System.out.println("before insert the list is " +l1);
+		//replace any values and print
 		l1.set(1, 9);   //replacing the element
 		System.out.println("after inserting the list is" +l1);
 		
